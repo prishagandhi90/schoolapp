efb51e34731b52135e651634df403e667300da96
harshilg <pd@venushospital.in>
2024-09-27 18:01:22 +0530

    getx issue solved

--- lib/app/moduls/dashboard/screen/dashboard1_screen.dart
+++ lib/app/moduls/dashboard/screen/dashboard1_screen.dart
@@ -21,65 +21,63 @@ class Dashboard1Screen extends GetView<DashboardController> {
         ? Get.find<DashboardController>() // If already registered, find it
         : Get.put(DashboardController());
     // return GetBuilder<DashboardController>(builder: (controller) {
-    return Obx(
-      () => SafeArea(
-        child: Scaffold(
-          resizeToAvoidBottomInset: true,
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: AppColor.white,
+        onDrawerChanged: (isop) {
+          // var bottomBarController = Get.put(BottomBarController());
+          final bottomBarController = Get.isRegistered<BottomBarController>()
+              ? Get.find<BottomBarController>() // If already registered, find it
+              : Get.put(BottomBarController());
+          hideBottomBar.value = isop;
+          bottomBarController.update();
+        },
+        drawer: CustomDrawer(),
+        appBar: AppBar(
           backgroundColor: AppColor.white,
-          onDrawerChanged: (isop) {
-            // var bottomBarController = Get.put(BottomBarController());
-            final bottomBarController = Get.isRegistered<BottomBarController>()
-                ? Get.find<BottomBarController>() // If already registered, find it
-                : Get.put(BottomBarController());
-            hideBottomBar.value = isop;
-            bottomBarController.update();
-          },
-          drawer: CustomDrawer(),
-          appBar: AppBar(
-            backgroundColor: AppColor.white,
-            title: Text(
-              AppString.venushospital,
-              style: TextStyle(
-                color: AppColor.primaryColor,
-                fontWeight: FontWeight.w700,
-                fontFamily: CommonFontStyle.plusJakartaSans,
-              ),
+          title: Text(
+            AppString.venushospital,
+            style: TextStyle(
+              color: AppColor.primaryColor,
+              fontWeight: FontWeight.w700,
+              fontFamily: CommonFontStyle.plusJakartaSans,
             ),
-            excludeHeaderSemantics: false,
-            leading: Builder(
-              builder: (context) {
-                return IconButton(
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  icon: Image.asset(
-                    AppImage.drawer,
-                    width: 20,
-                    color: AppColor.black,
-                  ),
-                );
-              },
-            ),
-            centerTitle: true,
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    Get.snackbar(
-                      AppString.comingsoon,
-                      '',
-                      colorText: Colors.white,
-                      backgroundColor: Colors.black,
-                      duration: const Duration(seconds: 1),
-                    );
-                  },
-                  icon: Image.asset(
-                    AppImage.notification,
-                    width: 20,
-                  ))
-            ],
           ),
-          body: const CustomGridview(),
+          excludeHeaderSemantics: false,
+          leading: Builder(
+            builder: (context) {
+              return IconButton(
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: Image.asset(
+                  AppImage.drawer,
+                  width: 20,
+                  color: AppColor.black,
+                ),
+              );
+            },
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Get.snackbar(
+                    AppString.comingsoon,
+                    '',
+                    colorText: Colors.white,
+                    backgroundColor: Colors.black,
+                    duration: const Duration(seconds: 1),
+                  );
+                },
+                icon: Image.asset(
+                  AppImage.notification,
+                  width: 20,
+                ))
+          ],
         ),
+        body: const CustomGridview(),
       ),
     );
     // });
