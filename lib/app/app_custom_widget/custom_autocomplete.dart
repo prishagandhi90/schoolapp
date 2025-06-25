2e5d15fdb8d18626b256bf3db7ad8d251d9ae33b
harshilg <pd@venushospital.in>
2025-06-25 10:31:21 +0530

    autocomplete common completed and implemented in medicationsheet but remaining in Investigation Requisition

--- lib/app/app_custom_widget/custom_autocomplete.dart
+++ lib/app/app_custom_widget/custom_autocomplete.dart
@@ -1,7 +1,11 @@
+import 'package:emp_app/app/core/util/app_color.dart';
+import 'package:emp_app/app/core/util/app_style.dart';
+import 'package:emp_app/app/moduls/invest_requisit/model/searchservice_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:emp_app/app/moduls/leave/screen/widget/custom_textformfield.dart';
 
-class CustomAutoComplete<T extends Object> extends StatelessWidget {
+class CustomAutoComplete<T extends SearchserviceModel> extends StatelessWidget {
   final Future<Iterable<T>> Function(TextEditingValue textEditingValue) optionsBuilder;
   final String Function(T option) displayStringForOption;
   final void Function(T selection) onSelected;
@@ -9,6 +13,7 @@ class CustomAutoComplete<T extends Object> extends StatelessWidget {
   final FocusNode? focusNode;
   final String hintText;
   final VoidCallback? onSuffixIconPressed;
+  final VoidCallback? onClearSuggestions; // Add callback to clear suggestions
 
   const CustomAutoComplete({
     Key? key,
@@ -19,41 +24,70 @@ class CustomAutoComplete<T extends Object> extends StatelessWidget {
     this.focusNode,
     this.hintText = 'Search',
     this.onSuffixIconPressed,
+    this.onClearSuggestions,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final node = focusNode ?? FocusNode();
-
     return Autocomplete<T>(
       displayStringForOption: displayStringForOption,
       optionsBuilder: optionsBuilder,
-      onSelected: onSelected,
+      onSelected: (T selection) {
+        onSelected(selection);
+        // controller.clear(); // Clear the controller on selection
+        if (onClearSuggestions != null) {
+          onClearSuggestions!(); // Clear suggestions if provided
+        }
+      },
       fieldViewBuilder: (context, textEditingController, localFocusNode, onEditingComplete) {
-        return TextFormField(
-          controller: textEditingController,
-          focusNode: node,
+        // Sync the provided controller with the Autocomplete's controller
+        if (controller != textEditingController) {
+          textEditingController.text = controller.text;
+          controller.addListener(() {
+            if (controller.text != textEditingController.text) {
+              textEditingController.text = controller.text;
+            }
+          });
+          textEditingController.addListener(() {
+            if (textEditingController.text != controller.text) {
+              controller.text = textEditingController.text;
+            }
+          });
+        }
+
+        return CustomTextFormField(
+          controller: textEditingController, // Use Autocomplete's controller
+          focusNode: focusNode ?? localFocusNode, // Use provided or Autocomplete's focus node
           decoration: InputDecoration(
             hintText: hintText,
-            prefixIcon: Icon(Icons.search, color: Colors.grey),
-            suffixIcon: controller.text.isNotEmpty
+            hintStyle: AppStyle.grey,
+            prefixIcon: Icon(Icons.search, color: AppColor.grey),
+            suffixIcon: textEditingController.text.isNotEmpty
                 ? IconButton(
-                    icon: Icon(Icons.cancel_outlined, color: Colors.black),
-                    onPressed: onSuffixIconPressed ??
-                        () {
-                          node.unfocus();
-                          controller.clear();
-                          SchedulerBinding.instance.addPostFrameCallback((_) {});
-                        },
+                    icon: const Icon(Icons.cancel_outlined, color: Colors.black),
+                    onPressed: () {
+                      textEditingController.clear();
+                      if (onSuffixIconPressed != null) {
+                        onSuffixIconPressed!();
+                      }
+                      if (onClearSuggestions != null) {
+                        onClearSuggestions!(); // Clear suggestions if provided
+                      }
+                      localFocusNode.unfocus();
+                      SchedulerBinding.instance.addPostFrameCallback((_) {});
+                    },
                   )
                 : null,
-            border: OutlineInputBorder(),
-            focusedBorder: OutlineInputBorder(
+            border: const OutlineInputBorder(),
+            focusedBorder: const OutlineInputBorder(
               borderSide: BorderSide(width: 0.8),
             ),
           ),
-          onFieldSubmitted: (_) => node.unfocus(),
-          onTapOutside: (_) => node.unfocus(),
+          onFieldSubmitted: (_) {
+            localFocusNode.unfocus();
+            onEditingComplete();
+          },
+          onTapOutside: (_) => localFocusNode.unfocus(),
         );
       },
     );
